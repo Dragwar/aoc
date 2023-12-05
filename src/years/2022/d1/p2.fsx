fd8d783787671e6703792b2729d07bdf9f41df6a
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2023-12-05 16:54:34 -0600

    2022 day 1 - both parts

---
+++ src/years/2022/d1/p2.fsx
@@ -0,0 +1,35 @@
+#load "../../Lib.fsx"
+
+open Lib
+open System
+
+type State = { Current: int; Elfs: int list }
+
+readInput __SOURCE_DIRECTORY__
+|> Seq.toList
+|> List.mapFold
+    (fun state line ->
+        match Int32.TryParse line |> Option.ofTryParse with
+        | Some(n) ->
+            (line,
+             {
+                 state with
+                     Elfs =
+                         (List.updateAt
+                             state.Current
+                             (n + state.Elfs.[state.Current])
+                             state.Elfs)
+             })
+        | None ->
+            (line,
+             {
+                 Current = state.Current + 1
+                 Elfs = state.Elfs @ [ 0 ]
+             }))
+    ({ Current = 0; Elfs = [ 0 ] })
+|> snd
+|> _.Elfs
+|> List.sortDescending
+|> List.take 3
+|> List.sum
+|> printfn "%A"
