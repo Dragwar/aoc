a32c5322db9ed4f55efc7d7cd97c1ce16b9ea0e2
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2023-12-05 18:00:40 -0600

    2022 day 2 - both parts

---
+++ src/years/2022/d2/p2.fsx
@@ -0,0 +1,110 @@
+#load "../../Lib.fsx"
+
+open Lib
+
+type Shape =
+    | Rock = 1
+    | Paper = 2
+    | Scissors = 3
+
+module Shape =
+    open System
+
+    let weight (shape: Shape) = int shape
+    let (|Weight|) shape = weight shape
+
+    let tryDecrypt =
+        function
+        | 'A'
+        | 'a'
+        | 'X'
+        | 'x' -> Some Shape.Rock
+        | 'B'
+        | 'b'
+        | 'Y'
+        | 'y' -> Some Shape.Paper
+        | 'C'
+        | 'c'
+        | 'Z'
+        | 'z' -> Some Shape.Scissors
+        | _ -> None
+
+    let decrypt =
+        tryDecrypt
+        >> Option.defaultWith (fun () ->
+            raise (NotSupportedException("Invalid Shape")))
+
+type Outcome =
+    | Loss = 0
+    | Draw = 3
+    | Win = 6
+
+module Outcome =
+    open Shape
+    open System
+    open System.Diagnostics
+
+    let weight (shape: Outcome) = int shape
+
+    let tryDecrypt =
+        function
+        | 'X'
+        | 'x' -> Some Outcome.Loss
+        | 'Y'
+        | 'y' -> Some Outcome.Draw
+        | 'Z'
+        | 'z' -> Some Outcome.Win
+        | x -> None
+
+    let decrypt =
+        tryDecrypt
+        >> Option.defaultWith (fun () ->
+            raise (NotSupportedException("Invalid Outcome")))
+
+
+    let fromRound (round: Shape * Shape) =
+        let (opp, player) = round
+        let playerWin = ((opp, Outcome.Loss), (player, Outcome.Win))
+        let playerLoss = ((opp, Outcome.Win), (player, Outcome.Loss))
+        let playerDraw = ((opp, Outcome.Draw), (player, Outcome.Draw))
+
+        match round with
+        | (Shape.Rock, Shape.Paper) -> playerWin
+        | (Shape.Rock, Shape.Scissors) -> playerLoss
+        | (Shape.Paper, Shape.Rock) -> playerLoss
+        | (Shape.Paper, Shape.Scissors) -> playerWin
+        | (Shape.Scissors, Shape.Rock) -> playerWin
+        | (Shape.Scissors, Shape.Paper) -> playerLoss
+        | (Weight(opp), Weight(player)) when opp = player -> playerDraw
+        | _ -> raise (UnreachableException("This shouldn't happen."))
+
+    let getDesiredShape (outcome: Outcome) (shape: Shape) : Shape =
+        match (shape, outcome) with
+        | (Shape.Rock, Outcome.Loss) -> Shape.Scissors
+        | (Shape.Rock, Outcome.Win) -> Shape.Paper
+        | (Shape.Paper, Outcome.Loss) -> Shape.Rock
+        | (Shape.Paper, Outcome.Win) -> Shape.Scissors
+        | (Shape.Scissors, Outcome.Loss) -> Shape.Paper
+        | (Shape.Scissors, Outcome.Win) -> Shape.Rock
+        | (opp, Outcome.Draw) -> opp
+        | _ -> raise (UnreachableException("This shouldn't happen."))
+
+
+let decryptedRounds (input: string array) =
+    input
+    |> Array.map (_.Split(" "))
+    |> Array.map (Array.map (char))
+    |> Array.map (fun x -> (Shape.decrypt x.[0], Outcome.decrypt x.[1]))
+
+
+// """
+// A Y
+// B X
+// C Z
+// """
+readInput __SOURCE_DIRECTORY__
+|> decryptedRounds
+|> Array.map (fun (opp, outcome) -> (opp, Outcome.getDesiredShape outcome opp))
+|> Array.map (Outcome.fromRound >> snd)
+|> Array.map (fun (s, o) -> (Shape.weight s) + (Outcome.weight o))
+|> Array.sum
